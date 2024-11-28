fa6d78760e0045dd77b822d8b9330707ef91cb96
Gabriel Pozo <jackgris2@gmail.com>
2024-11-28 11:21:52 -0300

    add: package to manage message broker connection Pub/Sub messages
    Signed-off-by: Gabriel Pozo <jackgris2@gmail.com>

---
+++ auth/pkg/msgbroker/msgbroker.go
@@ -0,0 +1,192 @@
+package msgbroker
+
+import (
+	"context"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-nats/v2/pkg/nats"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/jackgris/twitter-backend/auth/pkg/logger"
+	"github.com/jackgris/twitter-backend/auth/pkg/uuid"
+	nc "github.com/nats-io/nats.go"
+)
+
+type MsgBroker struct {
+	name string
+	sub  message.Subscriber
+	pub  message.Publisher
+	logs *logger.Logger
+}
+
+func NewMsgBroker(service, natsURL string, logs *logger.Logger) *MsgBroker {
+
+	marshaler := &nats.GobMarshaler{}
+	logger := watermill.NewStdLogger(false, false)
+	options := []nc.Option{
+		nc.RetryOnFailedConnect(true),
+		nc.Timeout(30 * time.Second),
+		nc.ReconnectWait(1 * time.Second),
+	}
+	subscribeOptions := []nc.SubOpt{
+		nc.DeliverAll(),
+		nc.AckExplicit(),
+	}
+
+	jsConfig := nats.JetStreamConfig{
+		Disabled:         false,
+		AutoProvision:    true,
+		ConnectOptions:   nil,
+		SubscribeOptions: subscribeOptions,
+		PublishOptions:   nil,
+		TrackMsgId:       false,
+		AckAsync:         false,
+		DurablePrefix:    "",
+	}
+
+	conn, err := nc.Connect(natsURL)
+	if err != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*400)
+		defer cancel()
+
+		logs.Error(ctx, service, "Failed to connect to Nats server", err)
+	}
+
+	configPub := nats.PublisherConfig{
+		URL:         natsURL,
+		NatsOptions: options,
+		Marshaler:   marshaler,
+		JetStream:   jsConfig,
+	}
+
+	publisher, err := nats.NewPublisher(configPub, logger)
+	if err != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*400)
+		defer cancel()
+
+		logs.Error(ctx, service, "publisher: connection to message broker", "status", err)
+	}
+
+	configSub := nats.SubscriberConfig{
+		URL:            natsURL,
+		CloseTimeout:   30 * time.Second,
+		AckWaitTimeout: 30 * time.Second,
+		NatsOptions:    options,
+		Unmarshaler:    marshaler,
+		JetStream:      jsConfig,
+	}
+
+	subscriber, err := nats.NewSubscriberWithNatsConn(conn, configSub.GetSubscriberSubscriptionConfig(), logger)
+	if err != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*400)
+		defer cancel()
+
+		logs.Error(ctx, service, "subscriber: connection to message broker", "status", err)
+	}
+
+	return &MsgBroker{name: service, pub: publisher, sub: subscriber, logs: logs}
+}
+
+func (m *MsgBroker) PublishMessages(topic string, msg *message.Message) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*400)
+	defer cancel()
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			m.logs.Info(ctx, m.name, "publish message", "status", "conxtext timeout", "message ID", msg.UUID)
+			break loop
+		default:
+			if err := m.pub.Publish(topic, msg); err != nil {
+				m.logs.Info(ctx, m.name, "publish message", "status", err, "topic", topic, "message ID", msg.UUID)
+			} else {
+				break loop
+			}
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func (m *MsgBroker) SubscribeGetFollowers(topic string) (<-chan *message.Message, error) {
+
+	ctx := context.Background()
+	messages, err := m.sub.Subscribe(ctx, topic)
+	if err != nil {
+
+		m.logs.Error(ctx, m.name, "subscriber: can't subscribe to "+topic, "status", err)
+		return nil, err
+	}
+
+	return messages, err
+
+}
+
+func (m *MsgBroker) Close() {
+	m.sub.Close()
+}
+
+type Header struct {
+	ID            string `json:"id"`
+	EventName     string `json:"event_name"`
+	CorrelationID string `json:"correlation_id"`
+	PublishedAt   string `json:"published_at"`
+}
+
+func NewHeader(eventName string) Header {
+	return Header{
+		ID:          uuid.New(),
+		EventName:   eventName,
+		PublishedAt: time.Now().Format(time.RFC3339),
+	}
+}
+
+type MockPublisher struct{}
+
+func (m *MockPublisher) Publish(topic string, messages ...*message.Message) error {
+	return nil
+}
+
+func (m *MockPublisher) Close() error {
+	return nil
+}
+
+type MockSubscriber struct{}
+
+func (m *MockSubscriber) Subscribe(ctx context.Context, topic string) (<-chan *message.Message, error) {
+	return nil, nil
+}
+
+func (m *MockSubscriber) Close() error {
+	return nil
+}
+
+func NewMockMsgBroker(logs *logger.Logger) *MsgBroker {
+	return &MsgBroker{
+		pub:  &MockPublisher{},
+		sub:  &MockSubscriber{},
+		logs: logs,
+	}
+}
+
+/*
+func (m *MsgBroker) PublishMessages(topic string, msg *message.Message) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*400)
+	defer cancel()
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			m.logs.Info(ctx, "auth service", "publish message", "conxtext timeout", "message ID", msg.UUID)
+			break loop
+		default:
+			if err := m.pub.Publish(topic, msg); err != nil {
+				m.logs.Info(ctx, "auth service", "", "publish message error", err, "message ID", msg.UUID)
+			} else {
+				break loop
+			}
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+*/
